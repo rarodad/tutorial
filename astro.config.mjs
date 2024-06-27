1762ab99058221a2dfe506ecf8d45adb27b22539
rarodad <morris9299@gmail.com>
2024-06-27 15:49:37 +0900

    튜토리얼 완료

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
-
-
+// https://astro.build/config
 export default defineConfig({
-    site: "https://9299.netlify.app"
+  site: "https://9299.netlify.app",
+  integrations: [preact()]
 });
